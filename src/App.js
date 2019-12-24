60c68e5030525af601cfef2053c0affefe652e70
unknown <ms.pishbin2@outlook.com>
2019-12-24 10:59:01 +0330

    addMetroInstations

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import Map from './components/map/Map';
 
--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,6 +1,8 @@
-import React,{useState} from 'react'
-//import React,{useState} from 'react';
-import ReactMapGL from 'react-map-gl';
+
+import React,{useState} from 'react';
+import ReactMapGL, { Marker } from 'react-map-gl';
+import * as MetroInstations from './MetroInstations.json';
+import { FaMapMarker } from 'react-icons/fa';
 
 
 
@@ -13,12 +15,31 @@ const Map = () => {
     zoom: 10
 
   })
+  const showInf = (MetroInstation) => (
+    alert(MetroInstation.title)
+    
+  )
+
+  
+
   return (
     <ReactMapGL
       {...viewport}
       onViewportChange={(viewport) => setViewport(viewport)}
       mapboxApiAccessToken="pk.eyJ1IjoibnBiOTgiLCJhIjoiY2s0YjVjNWNjMGE0dzNlcXFkNW45NWwwbSJ9.X2z-GTmU_fEQcKWBHAaqLA"
-    />
+    >
+      {
+        MetroInstations.Records.map((MetroInstation) => (
+          <Marker
+            key={MetroInstation.id}
+            latitude={Number(MetroInstation.latitude)}
+            longitude={Number(MetroInstation.longitude)}
+          >
+            <FaMapMarker color="blue" size="35" onClick={()=>showInf(MetroInstation)} />
+          </Marker>
+        ))
+      }
+    </ReactMapGL>
   )
 }
 
